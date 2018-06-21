8b5abfe440638b97c956ce275461d0c57bb2a784
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-06-21 14:29:49 -0500

    rollback

--- src/test/java/com/monotonic/testing/bdd/RunCucumberTest.java
+++ src/test/java/com/monotonic/testing/bdd/RunCucumberTest.java
@@ -5,6 +5,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }, features = "src/test/features", glue = "com.monotonic.testing.bdd")
+@CucumberOptions(
+    plugin = { 
+        "pretty", 
+        "json:target/cucumber-reports/Cucumber.json",
+        "junit:target/cucumber-reports/Cucumber.xml" }, 
+    features = "src/test/features", 
+    glue = "com.monotonic.testing.bdd")
 public class RunCucumberTest {
 }
